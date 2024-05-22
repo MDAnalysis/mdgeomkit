f3b3a9c7a3af6ca41939ef663abc90b4a6d180f0
Oliver Beckstein <orbeckst@gmail.com>
2024-05-22 15:36:47 -0700

    new info module
    - extract data for simulations (eg table of sims in Methods)
    - function for a single universe: extract(u)
    - function for printing prettytable from multiple universes summary(u1, u2, ...)
    - add tests

---
+++ mdgeom/info.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Information about simulations
+=============================
+
+This module contains functions that provide overview information about a
+trajectory that may be useful for a Methods section of a paper.
+
+"""
+import prettytable
+
+
+def summary(*universes, labels=None):
+    """Print summary information about all `universes`.
+
+    Parameters
+    ----------
+    universes : list of Universe
+        Print information about these simulation systems
+        in a table.
+
+    labels: list of str, optional
+        Add a label to the output, e.g., the name or ID of the simulation.
+        There needs to be one element on `labels` for each universe.
+
+    """
+    data_column_keys  = ["n_atoms", "Lx", "Ly", "Lz", "alpha", "beta", "gamma",
+                         "n_frames", "totaltime", "dt"]
+    data_column_names = ["n_atoms", "Lx/Å", "Ly/Å", "Lz/Å", "alpha", "beta", "gamma",
+                         "n_frames", "totaltime/ns", "dt/ps"]
+
+    if len(labels) != len(universes):
+        raise ValueError(f"labels {labels} must contain one "
+                         f"label per universe {universes}")
+
+    if labels is None:
+        labels = len(universes) * [None]
+        column_names = data_column_names
+    else:
+        column_names = ["simulation"] + data_column_names
+
+
+    table = prettytable.PrettyTable()
+    table.field_names = column_names
+
+    for u, label in zip(universes, labels):
+        data = extract(u)
+        data['totaltime'] /= 1000 # convert ps to ns
+        row = [data[key] for key in data_column_keys]
+        if label is not None:
+            row = [label] + row
+        table.add_row(row)
+
+    print(table)
+
+
+def extract(u):
+    """Extract summary information from a universe.
+
+    The information is returned as a dictionary for keys
+    - `n_atoms`: number of atoms
+    - `Lx`, `Ly`, `Lz`: length of the unit cell in Å (from first frame of the
+      trajectory)
+    - `alpha`, `beta`, `gamma`: angles of the unit cell in degrees
+    - `n_frames`: number of frames in the trajectory
+    - `totaltime`: total simulation time in ps
+    - `dt`: time between saved frames in the trajectory in ps
+
+
+    Parameters
+    ----------
+    u : Universe
+        MDAnalysis Universe.
+
+    Returns
+    -------
+    data : dict
+
+    """
+
+    try:
+        Lx, Ly, Lz, alpha, beta, gamma = u.dimensions
+    except TypeError:
+        # universe without a regular box
+        Lx, Ly, Lz, alpha, beta, gamma = 0, 0, 0,  0, 0, 0
+    data = {
+        'n_atoms': u.atoms.n_atoms,
+        'Lx': Lx, 'Ly': Ly, 'Lz': Lz,
+        'alpha': alpha, 'beta': beta, 'gamma': gamma,
+        'n_frames': u.trajectory.n_frames,
+        'totaltime': u.trajectory.totaltime,
+        'dt': u.trajectory.dt,
+    }
+    return data
+
+
+
+
---
+++ mdgeom/tests/test_info.py
@@ -0,0 +1,159 @@
+import pytest
+
+import MDAnalysis as mda
+from MDAnalysisTests import datafiles as data
+
+from mdgeom import info
+
+### simple testing
+
+def test_extract_ugmx():
+    """Test extract() function for the universe with the standard
+    GROMACS test trajectory.
+    """
+    reference = {'n_atoms': 47681,
+                 'Lx': 80.017006,
+                 'Ly': 80.017006,
+                 'Lz': 80.017006,
+                 'alpha': 60.0,
+                 'beta': 60.0,
+                 'gamma': 90.0,
+                 'n_frames': 10,
+                 'totaltime': 900.0000686645508,
+                 'dt': 100.00000762939453}
+    u = mda.Universe(data.TPR, data.XTC)
+
+    # run the extract() function
+    udata = info.extract(universe)
+
+    # assert udata == reference
+
+    # Floating point number comparisons have to be done carefully;
+    # https://docs.pytest.org/en/4.6.x/reference.html#pytest-approx
+    # works also for integers
+
+    for key in reference:
+        assert udata[key] == pytest.approx(reference[key])
+
+
+
+
+### testing with fixtures
+
+@pytest.fixture
+def u_gmx():
+    # universe together with the expected metadata
+    udata = {'n_atoms': 47681,
+             'Lx': 80.017006,
+             'Ly': 80.017006,
+             'Lz': 80.017006,
+             'alpha': 60.0,
+             'beta': 60.0,
+             'gamma': 90.0,
+             'n_frames': 10,
+             'totaltime': 900.0000686645508,
+             'dt': 100.00000762939453}
+    return mda.Universe(data.TPR, data.XTC), udata
+
+@pytest.fixture
+def u_charmm():
+    udata = {'n_atoms': 3341,
+             'Lx': 0,
+             'Ly': 0,
+             'Lz': 0,
+             'alpha': 0,
+             'beta': 0,
+             'gamma': 0,
+             'n_frames': 98,
+             'totaltime': 96.9999914562418,
+             'dt': 0.9999999119200186}
+    return mda.Universe(data.PSF, data.DCD), udata
+
+@pytest.fixture
+def u_pdb():
+    udata = {'n_atoms': 47681,
+             'Lx': 80.017,
+             'Ly': 80.017,
+             'Lz': 80.017,
+             'alpha': 60.0,
+             'beta': 60.0,
+             'gamma': 90.0,
+             'n_frames': 1,
+             'totaltime': 0.0,
+             'dt': 1.0}
+    return mda.Universe(data.PDB), udata
+
+
+def test_extract_gmx_fixture(u_gmx):
+    universe, reference = u_gmx
+    udata = info.extract(universe)
+
+    for key in reference:
+        assert udata[key] == pytest.approx(reference[key])
+
+
+# test where we capture stdout via capsys
+# https://docs.pytest.org/en/4.6.x/capture.html
+def test_summary(u_gmx, u_charmm, u_pdb, capsys):
+    u1 = u_gmx[0]    # just get the universe, ignore the data
+    u2 = u_charmm[0]
+    u3 = u_pdb[0]
+    labels = ["Adk GROMACS", "ADK DIMS", "Adk PDB"]
+    info.summary(u1, u2, u3, labels=labels)
+    captured = capsys.readouterr()
+
+    assert "+-------------+---------+-----------+-----------+-----------+-------+------+-------+----------+--------------------+--------------------+" in captured.out
+    for label in labels:
+        assert label in captured.out
+    assert "47681" in captured.out
+    assert "3341" in captured.out
+    assert "Lx/Å" in captured.out
+
+
+### parametrized testing
+
+@pytest.mark.parametrize("topology,trajectory,reference",
+                         [(data.TPR, data.XTC,
+                           {'n_atoms': 47681,
+                            'Lx': 80.017006,
+                            'Ly': 80.017006,
+                            'Lz': 80.017006,
+                            'alpha': 60.0,
+                            'beta': 60.0,
+                            'gamma': 90.0,
+                            'n_frames': 10,
+                            'totaltime': 900.0000686645508,
+                            'dt': 100.00000762939453}
+                           ),
+                          (data.PSF, data.DCD,
+                           {'n_atoms': 3341,
+                            'Lx': 0,
+                            'Ly': 0,
+                            'Lz': 0,
+                            'alpha': 0,
+                            'beta': 0,
+                            'gamma': 0,
+                            'n_frames': 98,
+                            'totaltime': 96.9999914562418,
+                            'dt': 0.9999999119200186}),
+                          (data.PDB, None,
+                           {'n_atoms': 47681,
+                            'Lx': 80.017,
+                            'Ly': 80.017,
+                            'Lz': 80.017,
+                            'alpha': 60.0,
+                            'beta': 60.0,
+                            'gamma': 90.0,
+                            'n_frames': 1,
+                            'totaltime': 0.0,
+                            'dt': 1.0}),
+                          ])
+def test_extract(topology, trajectory, reference):
+    if trajectory is not None:
+        universe = mda.Universe(topology, trajectory)
+    else:
+        universe = mda.Universe(topology)
+    udata = info.extract(universe)
+
+    for key in reference:
+        assert udata[key] == pytest.approx(reference[key])
