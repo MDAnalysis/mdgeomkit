fc4a0e940853d2003c82231f422963eaae6ed7c4
Oliver Beckstein <orbeckst@gmail.com>
2024-05-22 18:03:30 -0700

    distance calculations with PBC (when available)

---
+++ mdgeom/distances.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+r"""
+Distance-based analysis
+=======================
+
+One of the most fundamental analysis techniques is to quantify the interactions
+between different molecules or parts of molecules by calculating distances.
+
+The use of *periodic boundary conditions* (PBC) in MD simulations can make
+distance calculations challenging. We are typically interested in distances
+that obey the **minimum image convention**, i.e., the shortest distance taking
+all periodic images into account. MDAnalysis contains powerful distance
+calculation functions that automatically apply the minimum image convention
+when supplied with the unit cell dimensions (typically named `box`, i.e., the
+lengths of the box :math:`L_x`, :math:`L_y`, and :math:`L_z` and the three
+angles :math:`alpha`, :math:`beta`, :math:`gamma`).
+
+"""
+import numpy as np
+
+from MDAnalysis.lib.distances import calc_bonds, minimize_vectors
+
+
+def minimage_distance(a, b):
+    r"""Calculate distances between atoms |r_a - r_b|.
+
+    Compute the distances between corresponding atoms in two groups with the
+    same number of atoms :math:`|\mathbf{r}^{a}_{i} - \mathbf{r}^{b}_{i}|` and
+    return a numpy array of the distances of shape `(N,)`.
+
+    Periodic boundary conditions are automatically taken into account.
+
+    Parameters
+    ----------
+    a: AtomGroup
+        First group of N atoms.
+    b: AtomGroup
+        Second group of N atoms.
+
+    Returns
+    -------
+    distances : numpy.ndarray
+
+    Raises
+    ------
+    ValueError
+        If `a` and `b` have different sizes or if the box dimensions of the
+        two groups differ.
+
+
+    See Also
+    --------
+    MDAnalysis.lib.distances.calc_bonds
+
+    """
+
+    _check_groups(a, b)
+
+    d = calc_bonds(a, b, box=a.dimensions)
+    return d
+
+def minimage_difference_vector(a, b):
+    r"""Calculate the minimum image difference vector r_a - r_b
+
+    Compute the distance vector between corresponding atoms
+    :math:`\mathbf{r}^{a}_{i} - \mathbf{r}^{b}_{i}` in two groups with the same
+    number of atoms and return a numpy array of the distance vectors of shape
+    `(N, 3)`.
+
+    Periodic boundary conditions are automatically taken into account.
+
+    Parameters
+    ----------
+    a: AtomGroup
+        First group of N atoms.
+    b: AtomGroup
+        Second group of N atoms.
+
+    Returns
+    -------
+    distance_vectors : numpy.ndarray
+
+    Raises
+    ------
+    ValueError
+        If `a` and `b` have different sizes or if the box dimensions of the
+        two groups differ.
+
+
+    See Also
+    --------
+    MDAnalysis.lib.distances.minimize_vectors
+
+    """
+
+    # ensure that a.dimensions and b.dimensions are the same
+    _check_groups(a, b)
+
+    diff_vec = a.positions - b.positions
+    if a.dimensions is not None:
+        # need minimum image
+        r = minimize_vectors(diff_vec, a.dimensions)
+    else:
+        # no box information, so the difference vector is sufficient
+        r = diff_vec
+    return r
+
+
+def _check_groups(a, b):
+    """Helper function that checks compatibility of two atomgroups for
+    distance calculations
+
+    Raises :exc:`ValueError` if the groups contain different number of atoms or
+    have different associated unit cell dimensions.
+    """
+
+    if a.n_atoms != b.n_atoms:
+        raise ValueError("AtomGroups a and b contain different number of atoms")
+
+    if a.dimensions is None and b.dimensions is None:
+        # it's ok when both AtomGroups have no unitcell
+        pass
+    elif (a.dimensions is None) != (b.dimensions is None): # xor
+        raise ValueError("One AtomGroup does not have unit cell information")
+    elif not np.allclose(a.dimensions, b.dimensions):
+        raise ValueError("Unit cell dimensions differ between groups.")
+
+
---
+++ mdgeom/tests/test_distances.py
@@ -0,0 +1,83 @@
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+
+from mdgeom import distances
+
+# ADK FRET pairs
+# see doi: 10.1016/j.jmb.2009.09.009.
+fret_pairs = [
+    ("resname ALA and resid 55", "resname VAL and resid 169"),
+    ("resname ALA and resid 127", "resname ALA and resid 194"),
+    ("resname ILE and resid 52", "resname LYS* and resid 145")]
+
+
+@pytest.fixture
+def u_nopbc():
+    # simulation without periodic boundaries
+    # (box == None)
+    return mda.Universe(data.PSF, data.DCD)
+
+
+@pytest.fixture
+def u_pbc():
+    # simulation with periodic boundaries and protein split
+    # across unit cell
+    return mda.Universe(data.TPR, data.XTC)
+
+
+#
+# Tests for distances.minimage_distance()
+#
+
+def _minimage_distance(u):
+    # helper function
+    a = sum(u.select_atoms(pair[0] + " and name CA") for pair in fret_pairs)
+    b = sum(u.select_atoms(pair[1] + " and name CA") for pair in fret_pairs)
+
+    r = distances.minimage_distance(a, b)
+
+    return r
+
+def test_minimage_distance_nopbc(u_nopbc):
+    reference = np.array([12.91501288, 30.57278011, 28.92306815])
+    r = _minimage_distance(u_nopbc)
+    assert_allclose(r, reference)
+
+def test_minimage_distance_pbc(u_pbc):
+    reference = np.array([29.67992244, 38.36642607, 42.18877151])
+    r = _minimage_difference_vector(u_pbc)
+    assert_allclose(r, reference)
+
+
+#
+# Tests for distances.minimage_difference_vector()
+#
+
+def _minimage_difference_vector(u):
+    # helper function
+    a = sum(u.select_atoms(pair[0] + " and name CA") for pair in fret_pairs)
+    b = sum(u.select_atoms(pair[1] + " and name CA") for pair in fret_pairs)
+
+    r = distances.minimage_difference_vector(a, b)
+
+    return r
+
+
+def test_minimage_difference_vector_nopbc(u_nopbc):
+    reference = np.array(
+        [[ -6.7521524,  -7.5131125,  -8.047306 ],
+         [-21.687479 , -15.805498 , -14.646992 ],
+         [ 16.235527 , -19.996527 ,  13.156384 ]])
+    r = _minimage_difference_vector(u_nopbc)
+    assert_allclose(r, reference)
+
+
+def test_minimage_difference_vector_pbc(u_pbc):
+    reference = np.array(
+        [[-11.659996 ,   8.610001 ,  25.900003 ],
+         [ -9.439999 ,  37.177002 ,   0.8600006],
+         [-31.1585   ,  21.841507 , -18.220566 ]])
+    r = _minimage_difference_vector(u_pbc)
+    assert_allclose(r, reference)
+
